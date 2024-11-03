1aeda899491ae4ab2cbb23b1a6f3fd6e073992bd
Affey1 <AFFEY.ICEB@GMAIL.COM>
2024-11-03 18:21:25 -0300

    Update script.js

--- assets/js/global/script.js
+++ assets/js/global/script.js
@@ -1,18 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
-const poemDisplay = document.getElementById("poemDisplay"); // Seleciona a div para exibir o poema
 
 // Lógica para o menu hamburger
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 document.addEventListener('DOMContentLoaded', () => {
     const savedColor = localStorage.getItem('corSelecionada'); // Obtém a cor salva
-    const savedPoem = localStorage.getItem('poemaSelecionado'); // Obtém o poema salvo
 
     if (savedColor) {
         document.body.style.background = savedColor; // Aplica a cor ao fundo
-    }
-    if (savedPoem) {
-        poemDisplay.innerText = savedPoem; // Exibe o poema na div poemDisplay
     }
 });
