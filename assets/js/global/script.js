101000cb076c5d00008cb7ee6e9287f829e784a2
Affey1 <AFFEY.ICEB@GMAIL.COM>
2024-11-03 18:24:22 -0300

    Update script.js

--- assets/js/global/script.js
+++ assets/js/global/script.js
@@ -3,11 +3,3 @@ const nav = document.querySelector(".nav");
 
 // Lógica para o menu hamburger
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
-
-document.addEventListener('DOMContentLoaded', () => {
-    const savedColor = localStorage.getItem('corSelecionada'); // Obtém a cor salva
-
-    if (savedColor) {
-        document.body.style.background = savedColor; // Aplica a cor ao fundo
-    }
-});
